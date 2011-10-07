867964315814505531401206f427d9b27d23a1f2
Joe Gunn <gunnjo@curlycloud.com>
2011-10-07 12:27:37 -0400

    Fix home directories

--- config/chroot_local-includes/etc/iceweasel/pref/iceweasel.js
+++ config/chroot_local-includes/etc/iceweasel/pref/iceweasel.js
@@ -5,6 +5,3 @@ pref("intl.locale.matchOS", true);
 
 pref("browser.shell.checkDefaultBrowser", false);
 lockPref("xpinstall.enabled" ,false);
-pref("browser.startup.homepage", "http://www.commonde.org/start");
-pref("app.support.baseURL", "http://www.commonde.org/help?v=%VERSION%&o=%OS%");
-pref("extensions.getAddons.browseAddons", "http://www.commonde.org/add-ons");
