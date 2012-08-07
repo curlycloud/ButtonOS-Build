47e2d5ef6d129039d264ebf40d4a3a06dfd6b377
root <root@development.curlycloud.me>
2012-08-07 23:28:43 +0000

    Florence

---
+++ config/chroot_local-includes/etc/NetworkManager/dispatcher.d/10rehash.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+	echo No Interface 1>&2
+	exit 1
+fi
+
+PKGHOME=/tmp
+PKGFILE=$PHGHOME/lxde-cfg.tgz
+CFGURL="http://www.buttonos.org/load/themes/lxde-cfg.tgz"
+INSTFILE=/usr/share/lxde/defcfg.tgz
+
+case "$2" in
+	up)
+	(cd $PKGHOME ; curl -m 10 -s $CFGURL -O $PKGFILE.tmp &&
+		cd / ;  tar -xzf $PKGFILE.tmp ) &
+	;;
+	down)
+	;;
+	vpn-up|vpn-down)
+	;;
+	hostname|dhcp4-change|dhcp6-change)
+	;;
+esac
+
